fb9a0c1dc604bfc6340fa88f16f44cfff9e86cfb
rubyf <ruby@singular.net>
2018-05-10 23:38:14 -0700

    added all dimensions and metrics

--- singular_api_client/params.py
+++ singular_api_client/params.py
@@ -2,19 +2,61 @@ class Dimensions(object):
     APP = "app"
     SOURCE = "source"
     OS = "os"
-    # TODO: add all dimensions
+    SITE_PUBLIC_ID = "site_public_id"
+    PLATFORM = "platform"
+    COUNTRY_FIELD = "country_field"
+    ADN_CAMPAIGN_NAME = "adn_campaign_name"
+    ADN_CAMPAIGN_ID = "adn_campaign_id"
+    SINGULAR_CAMPAIGN_ID = "singular_campaign_id"
+    ADN_SUB_CAMPAIGN_NAME = "adn_sub_campaign_name"
+    ADN_SUB_CAMPAIGN_ID = "adn_sub_campaign_id"
+    ADN_SUB_ADNETWORK_NAME = "adn_sub_adnetwork_name"
+    ADN_ORIGINAL_CURRENCY = "adn_original_currency"
+    ADN_TIMEZONE = "adn_timezone"
+    ADN_UTC_OFFSET = "adn_utc_offset"
+    ADN_ACCOUNT_ID = "adn_account_id"
+    ADN_CAMPAIGN_URL = "adn_campaign_url"
+    ADN_STATUS = "adn_status"
+    ADN_CLICK_TYPE = "adn_click_type"
+    KEYWORD = "keyword"
+    PUBLISHER_ID = "publisher_id"
+    PUBLISHER_SITE_ID = "publisher_site_id"
+    PUBLISHER_SITE_NAME = "publisher_site_name"
+    ADN_CREATIVE_NAME = "adn_creative_name"
+    ADN_CREATIVE_ID = "adn_creative_id"
+    SINGULAR_CREATIVE_ID = "singular_creative_id"
+    CREATIVE_HASH = "creative_hash"
+    CREATIVE_IMAGE_HASH = "creative_image_hash"
+    CREATIVE_IMAGE = "creative_image"
+    CREATIVE_TEXT = "creative_text"
+    CREATIVE_URL = "creative_url"
+    CREATIVE_WIDTH = "creative_width"
+    CREATIVE_HEIGHT = "creative_height"
+    CREATIVE_IS_VIDEO = "creative_is_video"
+    TRACKER_NAME = "tracker_name"
+    RETENTION = "retention"
 
 
 class Metrics(object):
     ADN_IMPRESSIONS = "adn_impressions"
     ADN_COST = "adn_cost"
-    # TODO: add all metrics
+    ADN_ORIGINAL_COST = "adn_original_cost"
+    ADN_ESTIMATED_TOTAL_CONVERSIONS = "adn_estimated_total_conversions"
+    CUSTOM_CLICKS = "custom_clicks"
+    CUSTOM_INSTALLS = "custom_installs"
+    CTR = "ctr"
+    CVR = "cvr"
+    ECPI = "ecpi"
+    OCVR = "ocvr"
+    ECPM = "ecpm"
+    ECPC = "ecpc"
 
 
 class DiscrepancyMetrics(object):
     ADN_CLICKS = "adn_clicks"
     ADN_INSTALLS = "adn_installs"
-    # TODO: add all metrics
+    TRACKER_CLICKS = "tracker_clicks"
+    TRACKER_INSTALLS = "tracker_installs"
 
 
 class TimeBreakdown(object):
