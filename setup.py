3520637ed6c11f21101b35263ec48ae5530c84c8
rubyf <ruby@singular.net>
2018-05-14 22:38:04 -0700

    module config update

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from singular_api_client.version import __version__
 
 setup(
     name='singular_api_client',
     version=__version__,
-    py_modules=['singular_api_client'],
-    packages=['singular_api_client'],
+    py_modules=[],
+    packages=find_packages(),
     description='Helper Library to use Singular Reporting API',
     author='Ruby Feinstein',
     url='https://github.com/singular-labs/singular_api_client/',
     download_url='https://github.com/singular-labs/singular_api_client/tarball/%s' % __version__,
     keywords=[
         'singular'],
+    project_urls={
+        'Documentation': 'https://github.com/singular-labs/singular_api_client',
+        'Source': 'https://github.com/singular-labs/singular_api_client',
+        'Tracker': 'https://github.com/singular-labs/singular_api_client/issues',
+    },
     install_requires=[
         'requests',
         'pytz',
-        'retrying'
+        'retrying',
+        'futures'
     ])
